f2fd4557342a5cd5090e4126ee54caffcff0f1d6
Luifer De Pombo <2381303+depombo@users.noreply.github.com>
2024-01-29 10:34:01 -0800

    move pricing page into its own route

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import Pricing from '@/components/Pricing';
-import {
-  getSession,
-  getSubscription,
-  getActiveProductsWithPrices
-} from '@/app/supabase-server';
-
-export default async function PricingPage() {
-  const [session, products, subscription] = await Promise.all([
-    getSession(),
-    getActiveProductsWithPrices(),
-    getSubscription()
-  ]);
-
+export default async function UserDashboardPage() {
   return (
-    <Pricing
-      session={session}
-      user={session?.user}
-      products={products}
-      subscription={subscription}
-    />
+    <h1>Hi</h1>
   );
 }
