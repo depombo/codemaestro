02c932bccc7bac4bac48cd055e6759cbdc51da71
Luifer De Pombo <2381303+depombo@users.noreply.github.com>
2024-02-08 05:36:42 -0800

    add profile as a breadcrumb in sidebar

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link';
 import {
   getSession,
+  getUserDetails
 } from '@/app/actions';
-import { Logo } from '@/components/icons';
+import { IconSeparator, Logo } from '@/components/icons';
 
 import s from './Navbar.module.css';
 
 export default async function Navbar() {
   const session = await getSession();
+  const user = await getUserDetails();
 
   return (
     <nav className={s.root}>
@@ -15,20 +17,28 @@ export default async function Navbar() {
         <div className="flex flex-row justify-between align-center md:py-4">
           <div className="flex flex-row items-center">
             <Link href="/">
-              <Logo height="32" x="1" y="4" fill="white" stroke="zinc-700" />
+              <Logo height="24" x="1" y="4" fill="white" />
             </Link>
-            <Link href="/">
-
+            {/* <Link href="/">
               <p className='ml-2 font-bold'>CodeMaestro</p>
+            </Link> */}
+            <IconSeparator className="mx-4 size-6" stroke="rgb(113 113 122)" />
+            <Link href="/profile">
+              <img src={user?.avatar_url!} alt="User Avatar" className="w-6 h-6 mr-3 rounded-full" />
             </Link>
-
+            <Link href="/profile">
+              {user?.full_name}
+            </Link>
+            {/* <IconSeparator className="mx-4 size-6" stroke="rgb(113 113 122)" /> */}
           </div>
 
           <div className="flex justify-end flex-1 space-x-8 items-center align-center">
             <div className="ml-6 space-x-2 lg:block">
-              <Link href="/profile" className={s.link}>
-                {session ? "Profile" : 'Dashboard'}
-              </Link>
+              {!session && (
+                <Link href="/profile" className={s.link}>
+                  Dashboard
+                </Link>
+              )}
               <Link href="/pricing" className={s.link}>
                 Pricing
               </Link>
